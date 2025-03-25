1e9de21ca442765bbee048132756b822e27d5c69
pmieuzet <pauline.mieuzet@gmail.com>
2025-03-25 13:52:42 +0100

    ex01 wip

--- STARTING-0/ex01/format_ft_time.py
+++ STARTING-0/ex01/format_ft_time.py
@@ -1,4 +1,6 @@
 import time
 
+current_time = time.time()
 
-print("Seconds since January 1, 1970:", time.time(), "or")
+print("Seconds since January 1, 1970 :", current_time, "or", "{:.2e}".format(current_time), "in scientific notation")
+print(time.strptime(time.ctime(current_time), "%m %d %y"))
