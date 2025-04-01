1ff76b919c1126493b5e957000e6e821eed845a9
pmieuzet <pauline.mieuzet@gmail.com>
2025-04-01 22:38:56 +0200

    ex01

--- STARTING-0/ex00/Hello.py
+++ STARTING-0/ex00/Hello.py
@@ -4,10 +4,17 @@ ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
 
+# Lists are mutable sequences
 ft_list[1] = "World!"
+
+# Tuples are immutable sequences
 ft_tuple = ("Hello", "France!")
+
+# A set is an unordered collection with no duplicate elements
 ft_set.remove("tutu!")
 ft_set.add("Angouleme!")
+
+# Dict is standard mapping type and mapping are mutable objects that associates hashable values with arbitrary objects
 ft_dict["Hello"] = "42Angouleme!"
 
 
--- STARTING-0/ex01/format_ft_time.py
+++ STARTING-0/ex01/format_ft_time.py
@@ -1,6 +1,7 @@
-import time
+import datetime
 
-current_time = time.time()
+current_time = datetime.datetime.now()
+current_time_seconds = current_time.timestamp()
 
-print("Seconds since January 1, 1970 :", current_time, "or", "{:.2e}".format(current_time), "in scientific notation")
-print(time.strptime(time.ctime(current_time), "%m %d %y"))
+print("Seconds since January 1, 1970 :", current_time_seconds, "or", "{:.2e}".format(current_time_seconds), "in scientific notation")
+print(current_time.strftime("%b %d %Y"))
