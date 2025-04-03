33c9f0492cf17e6a283cbe46c2dc4e45db993195
pmieuzet <pauline.mieuzet@scub.net>
2025-04-03 13:57:04 +0200

    wip

--- STARTING-0/ex03/NULL_not_found.py
+++ STARTING-0/ex03/NULL_not_found.py
@@ -1,4 +1,22 @@
+import math
+
 def NULL_not_found(object: any) -> int:
-    typeObj = type(object)
-    if object == nan:
-        print("ok")
+    # match object:
+    #     case None:
+    #         print("Nothing:", object, type(object))
+    #         return 0
+    #     case nan:
+    #         print("Cheese:", object, type(object))
+    #         return 0
+    #     case 0: 
+    #         print("Zero:", object, type(object))
+    #         return 0
+    #     case '':
+    #         print("Empty", object, type(object))
+    #         return 0
+    #     case False:
+    #         print("Fake:", object, type(object))
+    #         return 0
+    #     case _:
+    #         print("Type not found")
+    #         return 1       
