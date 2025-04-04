b52cc877a1ff52c955b39ed088c7bb8fb8219194
pmieuzet <pauline.mieuzet@scub.net>
2025-04-04 13:46:26 +0200

    ex03

--- STARTING-0/ex03/NULL_not_found.py
+++ STARTING-0/ex03/NULL_not_found.py
@@ -1,22 +1,21 @@
 import math
 
 def NULL_not_found(object: any) -> int:
-    # match object:
-    #     case None:
-    #         print("Nothing:", object, type(object))
-    #         return 0
-    #     case nan:
-    #         print("Cheese:", object, type(object))
-    #         return 0
-    #     case 0: 
-    #         print("Zero:", object, type(object))
-    #         return 0
-    #     case '':
-    #         print("Empty", object, type(object))
-    #         return 0
-    #     case False:
-    #         print("Fake:", object, type(object))
-    #         return 0
-    #     case _:
-    #         print("Type not found")
-    #         return 1       
+    if object == None:
+        print("Nothing:", object, type(object))
+        return 0
+    elif isinstance(object, float) and object != object:
+        print("Cheese:", object, type(object))
+        return 0
+    elif isinstance(object, int) and object == 0:
+        print("Zero:", object, type(object))
+        return 0
+    elif isinstance(object, str) and object == '': 
+        print("Empty", object, type(object))
+        return 0
+    elif isinstance(object, bool) and object == False:
+        print("Fake:", object, type(object))
+        return 0
+    else :
+        print("Type not found")
+        return 1       
